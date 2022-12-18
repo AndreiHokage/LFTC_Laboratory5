8788a219936c9bbea5120aa39d8a04654421c0ec
AndreiHokage <andrei.balanici@yahoo.com>
2022-12-19 01:20:49 +0200

    finish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         /*
+
         Your job:
         1) build the production rules ;ist which you re reading from the input file
         2) build the Semnatic analyzwer object and display the information in a right way after you call the analyzer method
@@ -12,6 +13,14 @@ public class Main {
 
         Note: !!!
         Please send to the analyzer the start unfinished symbol as well!
+
          */
+
+        List<ProductionRule> ls = new ArrayList<>();
+        ls.add(new ProductionRule("S", "+SS"));
+        ls.add(new ProductionRule("S", "--S"));
+        ls.add(new ProductionRule("S", "a"));
+        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer("+a-aa", ls, "S");
+        semanticAnalyzer.analyze();
     }
 }
