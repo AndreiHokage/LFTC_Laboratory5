96275f49899b68f3006a858ab3f0101b5a29edd2
AndreiHokage <andrei.balanici@yahoo.com>
2022-12-19 01:15:35 +0200

    finish analyzer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,6 +9,9 @@ public class Main {
         1) build the production rules ;ist which you re reading from the input file
         2) build the Semnatic analyzwer object and display the information in a right way after you call the analyzer method
         3) some configurion input files
+
+        Note: !!!
+        Please send to the analyzer the start unfinished symbol as well!
          */
     }
 }
